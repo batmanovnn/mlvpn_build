f9e82835b85049a1eb6c91ac6d1347ccf6aa05fa
batmanovnn <batmanovnn@gmail.com>
2021-03-22 18:36:29 +0700

    build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github.com/zehome/MLVPN.git
+cd MLVPN
+sudo apt -y install build-essential make autoconf libev-dev libsodium-dev libpcap-dev pkg-config
+sudo ./autogen.sh
+sudo ./configure --enable-filters LDFLAGS="-Wl,-Bdynamic" --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+sudo ./makemake
+sudo make install
+sudo mkdir /etc/mlvpn
+sudo cp /doc/examples/mlvpn.init.d /etc/init.d/mlvpn
+sudo cp /doc/examples/mlvpn.default /etc/default/mlvpn
+sudo mkdir /usr/local/etc/mlvpn
+sudo cp /doc/examples/mlvpn_updown.sh /usr/local/etc/mlvpn/
+sudo cp /doc/examples/mlvpn.conf /etc/mlvpn/
+sudo chmod 600 /etc/mlvpn/mlvpn.conf
+sudo chmod 700 /etc/mlvpn/mlvpn_updown.sh
